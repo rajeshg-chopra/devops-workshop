946104c4008ed6f7deae61edf8a1f574dfe93ef1
rajeshgchopra <rajeshg.chopra@gmail.com>
2023-08-30 06:21:36 -0400

    version 2 add security group in terracode to open 22

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-051f7e7f6c2f40dc1"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "rajawskey"
     security_groups = [ "demo-sg" ]
 }
 
@@ -14,7 +14,7 @@ resource "aws_security_group" "demo-sg" {
   description = "SSH Access"
   
   ingress {
-    description      = "Shh access"
+    description      = "ssh access"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
@@ -24,13 +24,12 @@ resource "aws_security_group" "demo-sg" {
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "ssh-prot"
+    Name = "ssh-port"
 
   }
 }
